2556f0ba62ffd49102c6c083d46b7ad956480a1d
CodeLoca <156666617+CodeLoca@users.noreply.github.com>
2024-10-18 10:14:45 +0200

    feat:
    added the counter variable

--- exercise-001/main.cpp
+++ exercise-001/main.cpp
@@ -10,10 +10,16 @@ auto main(int argc, char **argv) -> int
      * CLI11 is a command line parser to add command line options
      * More info at https://github.com/CLIUtils/CLI11#usage
      */
-    CLI::App app{PROJECT_NAME};
+
+    auto counter{20}; /*set a default of 20*/
+
+    CLI::App app{PROJECT_NAME}; /*create an instance of an App with the variable name app*/
     try
     {
+
         app.set_version_flag("-V,--version", fmt::format("{} {}", PROJECT_VER, PROJECT_BUILD_DATE));
+        /*add a command line option to the app and fill the variable counter*/
+        app.add_option("-c,--count", counter, "How many times to insert");
         app.parse(argc, argv);
     }
     catch (const CLI::ParseError &e)
@@ -21,14 +27,34 @@ auto main(int argc, char **argv) -> int
         return app.exit(e);
     }
 
+    std::vector<int> numbers(counter);
+
+    srand(time(0));
+
+    for (int &number : numbers)
+    {
+        number = rand() % 100 +1;
+    }
+
     /**
      * The {fmt} lib is a cross platform library for printing and formatting text
      * it is much more convenient than std::cout and printf
      * More info at https://fmt.dev/latest/api.html
      */
-    fmt::print("Hello, {}!\n", app.get_name());
 
-    /* INSERT YOUR CODE HERE */
+    fmt::print("Numbers: ");
+    for (const auto &number : numbers)
+    {
+        fmt::print("{} ", number);
+    }
+
+    fmt::print("\nHello, {}!\n", app.get_name());
+
+    fmt::print("Count, {}!\n", counter);
+
+    fmt::print("Value of argc: {}\n", argc);
+
+    fmt::print("Value of argv: {}\n", argv[1]); /*print the first argument of argv*/
 
     return 0; /* exit gracefully*/
 }
